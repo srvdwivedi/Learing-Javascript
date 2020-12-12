8bf4d42c0e6650657d77ea8b8d1e24c769dfc614
srv.dwivedi <sourav.dwivedi@auw.co.in>
2020-12-12 13:25:31 +0530

    .

--- BasicsJS/index.js
+++ BasicsJS/index.js
@@ -1,3 +1,8 @@
 function text_change() {
     document.getElementById('text-change').innerHTML = "Good to see you here !";
-    }
+}
+
+var subjects = {language : 'JS', year : '2020', text : 'So lets get started.'};
+
+document.getElementById('subject').innerHTML =  "We will be learnig " + subjects.language + " ," + subjects.text;
+
